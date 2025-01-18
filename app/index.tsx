fec0b0030fb4b869486435596a0e2afff50c327d
dan <daro.albis.up@phinmaed.com>
2025-01-18 08:10:28 +0800

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+// app/index.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Halo</Text>
+    </View>
+  );
+}
---
+++ firebaseConfig.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore'; 
+import 'firebase/compat/functions';
+
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCsv3CsVJL4ri6Gz6I3UiCfxatoYVWoxbw',
+    authDomain: 'thesis-ad543.firebaseapp.com',
+    projectId: 'thesis-ad543',
+    storageBucket: 'thesis-ad543.appspot.com', 
+    messagingSenderId: '1836074086378738059',
+    appId: '1:885904173811:android:ef336df3417631e854a586',
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const db = firebase.firestore(); 
+export default firebase;
